f8ecfa5d61d36087478aea6f52b1c0a2f1ef6d85
Ali Osman Yılmaz <aliosmanyilmaz@ogr.iu.edu.tr>
2023-12-01 20:22:57 +0300

    Update tests.py with new unit tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from djqngo.test import TestCase, Client
+from django.urls import reverse
+import json
 
-# Create your tests here.
+class PowerFunctionTests(TestCase):
+
+    def setUp(self):
+        # Set up the client to make requests
+        self.client = Client()
+
+    def test_calculate_power_function(self):
+        # Test the calculate_power_function view
+        response = self.client.get(reverse('calculate_power_function'), {'n': 2})
+        self.assertEqual(response.status_code, 200)
+        
+        data = json.loads(response.content)
+        self.assertIn(' row[0][\'x\'][i]);
+        # Add more assertions here to validate the response data
