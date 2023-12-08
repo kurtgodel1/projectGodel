9ffbb4f19e97e8c66bc829b1c5c2ca63b7054318
Ali Osman Yılmaz <aliosmanyilmaz@ogr.iu.edu.tr>
2023-12-08 16:21:46 +0300

    lint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.urls import path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
