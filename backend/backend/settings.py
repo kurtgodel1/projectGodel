40c51dff53e9cb760461bc314028e12beb8d121f
Ali Osman Yılmaz <aliosmanyilmaz@ogr.iu.edu.tr>
2023-12-08 16:05:28 +0300

    linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -100,7 +100,6 @@ CACHES = {
 }
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 AUTH_USER_MODEL = 'auth_app.User'
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
 from django.conf import settings
 from django.urls import path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -47,9 +48,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
-
-
-
-
-
-
