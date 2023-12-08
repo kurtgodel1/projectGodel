5e601d6aa9204d3ff6e3983f86ddd6aa5dbc5cb7
Ali Osman Yılmaz <aliosmanyilmaz@ogr.iu.edu.tr>
2023-12-08 17:08:50 +0300

    Create Gruntfile

---
+++ frontend/Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      // Task configuration goes here.
+    });
+  
+    // Load plugins here.
+  
+    // Register tasks here.
+  };
