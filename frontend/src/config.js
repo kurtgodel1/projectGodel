b784d4f2afb83d2d6a251f7dd82fe9056f27cb79
Ali Osman Yılmaz <aliosmanyilmaz@ogr.iu.edu.tr>
2023-12-12 21:21:19 +0300

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,7 +4,7 @@ const dev = {
   };
   
   const prod = {
-    API_URL: 'https://projectgodel-9ba604cd888b.herokuapp.com/',
+    API_URL: 'https://projectgodel-9ba604cd888b.herokuapp.com',
   };
   
   const config = process.env.NODE_ENV === 'development' ? dev : prod;
