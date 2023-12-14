a5e52e15bfc8907e19f34503947e465cb590f85b
Ali Osman Yılmaz <aliosmanyilmaz@ogr.iu.edu.tr>
2023-12-14 21:54:55 +0300

    s

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -41,10 +41,6 @@ schema_view = get_schema_view(
 )
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-
 urlpatterns = [
     path('', health),
     path('admin/', admin.site.urls),
@@ -53,7 +49,6 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('sentry-debug/', trigger_error)
 ]
 
 if settings.DEBUG:
--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from 'react';
 import { Box, Container, Drawer, List, ListItem, Button, AppBar, Toolbar, Typography, IconButton, useTheme, useMediaQuery } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import { styled } from '@mui/system';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store'; // Update the path as needed
 import { LoginButton, RegisterButton, LogoutButton } from '../auth_components/AuthButtons'; // Update the path as needed
@@ -9,6 +8,8 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { styled, alpha } from '@mui/material/styles';
+
 
 const MainLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
     const theme = useTheme();
@@ -21,27 +22,43 @@ const MainLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
         setIsDrawerOpen(!isDrawerOpen);
     };
 
-    const DrawerContainer = styled(Drawer)({
+    const DrawerContainer = styled(Drawer)(({ theme }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
         '& .MuiDrawer-paper': {
             width: drawerWidth,
-            backgroundColor: theme.palette.grey[100],
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            backgroundColor: theme.palette.grey[200],
             color: theme.palette.text.primary,
         },
-    });
+    }));
 
-    const AppBarStyled = styled(AppBar)({
-        backgroundColor: theme.palette.background.default,
-        color: theme.palette.text.primary,
+    const AppBarStyled = styled(AppBar)(({ theme }) => ({
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
         boxShadow: 'none',
-        borderBottom: `1px solid ${theme.palette.divider}`,
-    });
+        borderBottom: `1px solid ${alpha(theme.palette.primary.light, 0.2)}`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    }));
 
 
     const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
 
     return (
         <Box sx={{ display: 'flex', width: '100%', minHeight: '100vh', bgcolor: 'background.default' }}>
-            <AppBarStyled position="fixed">
+            <AppBarStyled position="fixed" sx={{ width: `calc(100% - ${isDrawerOpen ? drawerWidth : 0}px)` }}>
                 <Toolbar>
                     <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
                         <MenuIcon />
